dcebbf02866faaa30bc63790a789fc339da293b2
tym <tym@marvin>
2009-09-13 18:34:15 +0100

    Added more add methods to deck class as well as methods to determine the multiverse id of a card if only its name is supplied.

--- src/org/randomcoding/mtg/tools/legalitychecker/deck/MtgDeck.java
+++ src/org/randomcoding/mtg/tools/legalitychecker/deck/MtgDeck.java
@@ -1,7 +1,13 @@
 package org.randomcoding.mtg.tools.legalitychecker.deck;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.randomcoding.mtg.tools.legalitychecker.scraper.GathererDataScraper;
 
 /**
  * A Class to represent simple data for a MTG deck
@@ -10,6 +16,8 @@ import java.util.Map;
  */
 public class MtgDeck
 {
+	private static final Log log = LogFactory.getLog(MtgDeck.class);
+
 	private final String deckName;
 	private final Map<MtgCardData, Integer> cardsAndCount;
 
@@ -28,13 +36,58 @@ public class MtgDeck
 	 */
 	public void add(String cardName, int cardMultiverseId, int cardCount)
 	{
-		MtgCardData cardData = getCardData(cardName);
-		if (cardData == null)
+		if (cardCount > 0 && cardMultiverseId > 0)
 		{
-			cardData = new MtgCardData(cardName, cardMultiverseId);
+			MtgCardData cardData = getCardData(cardName);
+
+			if (cardData == null)
+			{
+				cardData = new MtgCardData(cardName, cardMultiverseId);
+			}
+
+			updateCardCount(cardData, cardCount);
+		}
+	}
+
+	public void add(String cardName, int cardCount)
+	{
+		add(cardName, getMultiverseId(cardName), cardCount);
+	}
+
+	private int getMultiverseId(String cardName)
+	{
+		int multiverseId = -1;
+		try
+		{
+			multiverseId = new GathererDataScraper().getMultiverseId(cardName);
+		}
+		catch (IOException e)
+		{
+			log.error(e);
 		}
 
-		updateCardCount(cardData, cardCount);
+		return multiverseId;
+	}
+
+	public Set<MtgCardData> getCardData()
+	{
+		return cardsAndCount.keySet();
+	}
+
+	public int getCardCount(MtgCardData cardData)
+	{
+		Integer cardCount = cardsAndCount.get(cardData);
+		return (cardCount != null ? cardCount : -1);
+	}
+
+	public int getCardCount(String cardName)
+	{
+		return getCardCount(getCardData(cardName));
+	}
+
+	public String getDeckName()
+	{
+		return deckName;
 	}
 
 	private void updateCardCount(MtgCardData cardData, int cardCount)
@@ -64,17 +117,5 @@ public class MtgDeck
 		}
 
 		return cardData;
-	}
-
-	/**
-	 * Adds a single card to the deck
-	 * 
-	 * @param cardName The name of the card, as it is printed
-	 * @param cardMultiverseId The Gatherer MultiverseId of the card
-	 * @param card
-	 */
-	public void add(String cardName, int cardMultiverseId)
-	{
-		add(cardName, cardMultiverseId, 1);
 	}
 }
