3abd0ddb503e0960565489fb5edd782ac8bba863
tym <tym@marvin>
2009-09-15 23:05:02 +0100

    Added stub class for Legality Explanation Generator

---
+++ src/org/randomcoding/mtg/tools/legalitychecker/deck/DeckLegalityExplanationGenerator.java
@@ -0,0 +1,18 @@
+package org.randomcoding.mtg.tools.legalitychecker.deck;
+
+/**
+ * Class to explain why a deck is not legal in different formats
+ * <p>
+ * The basic rules are
+ * <ol>
+ * <li>Only one copy of a Restricted card is allowed</li>
+ * <li>No Banned cards are allowed</li>
+ * <li>A maximum of four copies of a single, non Restricted, card are allowed in a deck</li>
+ * </ol>
+ * 
+ * @author Tym The Enchanter
+ */
+public class DeckLegalityExplanationGenerator
+{
+
+}
